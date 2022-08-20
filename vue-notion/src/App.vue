41dab3998d61a30329746e733c6437b802173008
Yasushi Tamura <yasushitamura@YasushinoMacBook-Air.local>
2022-08-20 14:32:19 +0900

    Font Awesomeの導入

--- vue-notion/src/App.vue
+++ vue-notion/src/App.vue
@@ -1,16 +1,19 @@
 <template>
   <div id="app">
+    <!-- template部でコンポーネントをHTMLタグとして記述 -->
     <NavigationBar />
     <MainPage />
   </div>
 </template>
 
 <script>
+//script部でコンポーネントの読み込み
 import NavigationBar from '@/components/NavigationBar.vue'
 import MainPage from '@/components/MainPage.vue'
 
 export default {
   name: 'App',
+  //script部で使用するコンポーネントの宣言
   components: {
     NavigationBar,
     MainPage
