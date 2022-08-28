c57a6a1bcfa513bee6719b17ff2613b68a4ad436
Yasushi Tamura <yasushitamura@YasushinoMacBook-Air.local>
2022-08-28 23:24:49 +0900

    仕様変更

--- vue-notion/src/App.vue
+++ vue-notion/src/App.vue
@@ -1,28 +1,39 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <NavigationBar />
+    <router-view></router-view>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
+import NavigationBar from '@/components/NavigationBar.vue'
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    NavigationBar
   }
 }
 </script>
 
 <style>
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+  height: 100vh;
+}
+
+button.transparent {
+  margin: 5px;
+  background: transparent;
+  border: none;
+}
+
+input.transparent {
+  width: 100%;
+  border: none;
+}
+
+input.transparent:focus {
+  outline: none;
+  -webkit-box-shadow: none;
+  box-shadow: none;
 }
 </style>
